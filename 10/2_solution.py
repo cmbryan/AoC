41399454c50be47274aaf350d33f6135db5ac06e
Bryan Christopher (ETAS-VOS/XPC-Yo1) <christopher.bryan@etas.com>
2022-12-15 14:33:15 +0000

    10.2

--- 10/2_solution.py
+++ 10/2_solution.py
@@ -2,27 +2,25 @@ def parse_input(path):
     with open(path) as fh:
         idx = 0
         for line in fh.readlines():
-            idx += 1
             yield idx, 0
             if "noop" not in line:
                 idx += 1
                 yield idx, int(line.split()[1])
+            idx += 1
 
 def solution(path):
     reg = 1
     result = 0
     for inst in parse_input(path):
         cycle, val = inst
-        if (inst[0] - 20) % 40 == 0:
-            score = reg * cycle
-            result += score
+        sprite_vals = range(reg-1, reg+2)
+        x_pos = cycle % 40
+        print("#" if x_pos in sprite_vals else ".", end="")
+        if x_pos == 39:
+            print("")
         reg += val
-        if cycle > 220:
-            break
-    return result
 
-_t = solution("sample.txt")
-assert _t == 13140, _t
-print("Ok")
-
-print(f"Part 1 => {solution('input.txt')}")
+print("Testing")
+solution("sample.txt")
+print(f"Part 2 =>")
+solution('input.txt')
