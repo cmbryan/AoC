a5d47cf3ce9fd2ebdb3647055930f6dcfea50011
Bryan Christopher (ETAS-VOS/XPC-Yo1) <christopher.bryan@etas.com>
2022-12-17 07:57:31 +0000

    13.1 correct but too slow

---
+++ 12/solution.py
@@ -0,0 +1,57 @@
+from copy import copy
+
+
+def parse_input(path):
+    grid = []
+    with open(path) as fh:
+        for line in fh.readlines():
+            grid.append([ch for ch in line.strip()])
+    for row_ix, row in enumerate(grid):
+        for col_ix, col in enumerate(row):
+            if col == "S":
+                start = (row_ix, col_ix)
+                grid[row_ix][col_ix] = "a"
+            elif col == "E":
+                end = (row_ix, col_ix)
+                grid[row_ix][col_ix] = "z"
+            grid[row_ix][col_ix] = ord(grid[row_ix][col_ix])
+    return grid, start, end
+
+
+def solution(filepath):
+    grid, start, end = parse_input(filepath)
+    paths = [[start]]
+    while True:
+        updated_paths = []
+        for path in paths:
+            cur = path[-1]
+            # branches
+            proposed = [
+                (cur[0], cur[1] - 1),  # up
+                (cur[0], cur[1] + 1),  # down
+                (cur[0] - 1, cur[1]),  # left
+                (cur[0] + 1, cur[1]),  # right
+            ]
+            for prop in proposed:
+                if (
+                    prop[0] >= 0
+                    and prop[0] < len(grid)
+                    and prop[1] >= 0
+                    and prop[1] < len(grid[0])
+                    and prop not in path
+                    and grid[prop[0]][prop[1]] - grid[cur[0]][cur[1]] <= 1
+                ):
+                    # optimization: could check whether this position exists in any path
+                    updated_paths.append(copy(path) + [prop])
+
+        # Check for winner
+        for path in updated_paths:
+            if path[-1] == end:
+                return len(path) - 1  # Not counting the start
+        paths = updated_paths
+
+
+_t = solution("sample.txt")
+assert _t == 31, _t
+print("Ok")
+print(f"Part 1 => {solution('input.txt')}")
